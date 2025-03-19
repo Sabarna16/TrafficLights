bd22aed9af37b28dac6f28b07de8363776a0e3ac
Sabarna16 <sabarnaj2006@gmail.com>
2025-03-19 21:59:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var red_btn=document.getElementById("Red");
+var yellow_btn=document.getElementById("Yellow");
+var green_btn=document.getElementById("Green");
+var img=document.getElementById("Light");
+
+function Red_on(){
+    img.src="https://cdn.pixabay.com/photo/2022/07/26/14/22/semaphore-7345985_640.png"
+}
+function Yellow_on(){
+    img.src="https://cdn.pixabay.com/photo/2022/07/26/14/22/semaphore-7345986_1280.png"
+}
+function Green_on(){
+    img.src="https://cdn.pixabay.com/photo/2022/07/26/14/17/semaphore-7345974_960_720.png"
+}
